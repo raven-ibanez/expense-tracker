37f551ca5bd84ecbccc6a0cfbc2755264031b717
wayne <waynemerana@gmail.com>
2023-11-15 13:21:55 +0800

    wayne

--- src/wayne.js
+++ src/wayne.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 
 function wayne() {
   return (
 
-    <div>wayne
+    <div>wayne, pogi ba o hindi?
 
-        <h1>wayne hinahayaan</h1>
+<ButtonGroup aria-label="Basic example">
+      <Button variant="secondary">oo</Button>
+      <Button variant="secondary">omsim</Button>
+      <Button variant="secondary">tama</Button>
+    </ButtonGroup>
+
     </div>
 
 
