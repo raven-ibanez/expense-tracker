b7a279dcb8ea19c9b9312d4f5f9b28d3f1502378
unknown <mitwinter825@gmail.com>
2023-11-15 13:07:24 +0800

    eme

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import Maestro from './Maestro.js'
+import Morales from './morales.js'
+import Raven from './raven.js'
+import Wayne from './wayne.js'
+
+
+
 function App() {
   return (
     <div>App
 
-      <Maestro />
+     <Maestro/>
+     <Morales/>
+     <Raven/>
+     <Wayne/>
+
+
     </div>
   )
 }
